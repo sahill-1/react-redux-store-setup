8fea23214ee885d62f87e486c76c67fe0d6e5399
sahill-1 <sv260397@gmail.com>
2023-06-22 14:52:40 +0530

    counter by redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {createStore} from "redux";
+import {Provider, connect, useSelector, useDispatch, createStoreHook} from "react-redux";
+import { Component } from 'react';
 
+// FIRST REDUCER
+const countReducer = function(state=0, action) {
+  switch(action.type){
+    
+    case "ADD": return state+1;
+
+    case "SUBTRACT": return state-1;
+
+    default: return state;
+  }
+}
+
+// CREATING STORE
+const store = createStore(countReducer);
+
+//MAP STATE TO PROPS
+const mapStateToProps = state => {
+  return{
+    count:state
+  }
+}
+
+// MAP DISPATCH TO PROPS
+const mapDispatchToProps = dispatch => {
+  return {
+    add: () => dispatch({type:"ADD"}),
+    subtract: () => dispatch({type:"SUBTRACT"})
+  }
+}
+
+const Comp = ({count, add, subtract}) => {
+  return(
+    <>
+    <h1>Count = {count}</h1>
+    <button onClick={add}>ADD</button>
+    <button onClick={subtract}>SUBTRACT</button>
+    </>
+  )
+}
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Comp)
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Container />
+    </Provider>
   );
 }
 
